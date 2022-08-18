a79c6a4a8f6861c66ad9795792944f5b5eea6719
zakaria5729 <zakariahossain143@gmail.com>
2022-08-18 16:46:09 +0600

    explore android handler and runnable methods

---
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.androidconcurrency2020
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.util.Log
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidconcurrency2020.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize view binding for view object references
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize button click handlers
+        with(binding) {
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
+
+    }
+
+    /**
+     * Run some code
+     */
+    private fun runCode() {
+        val runnable = object: Runnable {
+            override fun run() {
+                log("Operation from runnable without lambda")
+            }
+        }
+
+        val handler = Handler()
+        handler.post(runnable)
+
+        Handler().post { log("Operation from runnable with lambda") }
+
+        Handler().postDelayed({ log("Operation from runnable 1") }, 3000)
+        Handler().postDelayed({ log("Operation from runnable 2") }, 2000)
+        Handler().postDelayed({ log("Operation from runnable 3") }, 1000)
+
+        Handler().postAtTime({ log("Operation from runnable 4") }, SystemClock.uptimeMillis() + 4000)
+
+        log("Synchronous operation 1")
+        log("Synchronous operation 2")
+        log("Synchronous operation 3")
+    }
+
+    /**
+     * Clear log display
+     */
+    private fun clearOutput() {
+        binding.logDisplay.text = ""
+        scrollTextToEnd()
+    }
+
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd() {
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+}
