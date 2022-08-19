1a1ad04880c84a28bc468e791c30dfcd6703bdae
zakaria5729 <zakariahossain143@gmail.com>
2022-08-19 11:54:49 +0600

    Switch between foreground and background threads in coroutine

--- app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -2,38 +2,19 @@ package com.example.androidconcurrency2020
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.util.Log
-import android.widget.ImageView
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
-import kotlin.random.Random
+import kotlinx.coroutines.*
+import java.net.URL
+import java.nio.charset.Charset
+
+const val fileUrl = "https://2833069.youcanlearnit.net/lorem_ipsum.txt"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var imageViews: Array<ImageView>
-
-    private val drawables = arrayOf(
-        R.drawable.die_1, R.drawable.die_2,
-        R.drawable.die_3, R.drawable.die_4,
-        R.drawable.die_5, R.drawable.die_6
-    )
-
-    private val handler = object: Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-
-            val bundle = msg.data
-            val dieIndex = bundle?.getInt(DIE_INDEX_KEY) ?: 0
-            val dieValue = bundle?.getInt(DIE_VALUE_KEY) ?: 1
-
-            Log.i(LOG_TAG, "index = $dieIndex, value = $dieValue")
-            imageViews[dieIndex].setImageResource(drawables[dieValue])
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,33 +23,57 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
-        binding.rollButton.setOnClickListener { rollTheDice() }
+        // Initialize button click handlers
+        with(binding) {
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
+
     }
 
-    private fun rollTheDice() {
-        for (dieIndex in imageViews.indices) {
-            thread(start = true) {
-                Thread.sleep(dieIndex * 10L)
-
-                val bundle = Bundle()
-                bundle.putInt(DIE_INDEX_KEY, dieIndex)
-
-                for (i in 1..10) {
-                    bundle.putInt(DIE_VALUE_KEY, getDieValue())
-
-                    Message().also {
-                        it.data = bundle
-                        handler.sendMessage(it)
-                    }
-
-                    Thread.sleep(150)
-                }
-            }
+    /**
+     * Run some code
+     */
+    private fun runCode() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = fetchSomething()
+            log(result ?: "Null")
         }
     }
 
-    private fun getDieValue(): Int {
-        return Random.nextInt(1, 6)
+    /**
+     * Clear log display
+     */
+    private fun clearOutput() {
+        binding.logDisplay.text = ""
+        scrollTextToEnd()
     }
+
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd() {
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+    private suspend fun fetchSomething(): String? {
+//        delay(2000)
+        log("Starting the request")
+        return withContext(Dispatchers.IO) {
+            val url = URL(fileUrl)
+            return@withContext url.readText(Charset.defaultCharset())
+        }
+    }
+
 }
+
